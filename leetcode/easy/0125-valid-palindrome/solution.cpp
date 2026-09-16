20ddf01132d0953b0722ffc6cf103b1931ffdaaa
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-16 11:13:36 +0600

    ✅ Valid Palindrome | Easy | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-09-16

---
+++ leetcode/easy/0125-valid-palindrome/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string s1 = "";
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(int(s[i] >= 97 && 122 >= int(s[i])))
+                s1 += s[i];
+            else if (int(s[i] >= 65 && 90 >= int(s[i])))
+            {
+                s1 += char(int(s[i])+ 32);
+            }
+
+
+        }
+
+        string s2 = s1;
+        reverse(s2.begin(),s2.end());
+
+
+        if(s1 == s2) return true ;
+        else return false;
+            
+    }
+};
