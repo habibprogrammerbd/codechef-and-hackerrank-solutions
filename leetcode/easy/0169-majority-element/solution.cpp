dcb0db0bfbb0fc2a3211ca9a417d6d3a52b28bc0
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-16 10:30:05 +0600

    ✅ Majority Element | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 42.2 MB | 2026-09-16

--- leetcode/easy/0169-majority-element/solution.cpp
+++ leetcode/easy/0169-majority-element/solution.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-
-        int x = 0,y = 0;
+        
         int ans = 0;
+        int count = 0;
 
         for(int i = 0; i < nums.size(); i++)
         {
-            if(nums[i] % 2 == 0) x++;
-            else y++;
-        }
-
-        if(x > y)
-        {
-            for(int i = 0; i < nums.size(); i++)
+            if(count == 0)
             {
-                if(nums[i] % 2 == 0) ans = nums[i];
-                break;
+                ans = nums[i];
             }
-        }
-        else
-        {
-            for(int i = 0; i < nums.size(); i++)
-            {
-                if(nums[i] % 2 != 0) ans = nums[i];
-                break;
-            }
-        }
 
+            if(ans == nums[i]) count++;
+            else count--; 
+                    
+    
+        }
         return ans;
     }
 };
